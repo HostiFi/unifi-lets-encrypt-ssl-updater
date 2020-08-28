39429d86e3d6b5329d6b262d638d80abf097b335
Reilly Chase <rchase@rchase.com>
2020-08-28 12:42:14 -0400

    Multidomain update

--- unifi-lets-encrypt-ssl-updater.sh
+++ unifi-lets-encrypt-ssl-updater.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-while getopts e:d: option
-do
-case "${option}"
-in
-e) EMAIL=${OPTARG};;
-d) HOSTNAMEVAR=${OPTARG};;
-esac
+while getopts e:d: option; do
+  case $option in
+    "d") DOMAINS+=("$OPTARG");;
+    "e") EMAIL=${OPTARG};;
+  esac
 done
+echo $EMAIL;
+for DOMAIN in "${DOMAINS[@]}"; do
+  stringprefix=" --domain "
+  stringpostfix=" "
+  DOMAINSTR+=$stringprefix$DOMAIN$stringpostfix
+done
+echo $DOMAINSTR;
 
-certbot --nginx --email $EMAIL --agree-tos --no-eff-email --domain $HOSTNAMEVAR --no-redirect
+certbot --nginx --email $EMAIL --agree-tos --no-eff-email --expand $DOMAINSTR --no-redirect
 wget -O /root/unifi-lets-encrypt-ssl-importer.sh https://raw.githubusercontent.com/HostiFi/unifi-lets-encrypt-ssl-importer/master/unifi-lets-encrypt-ssl-importer.sh
-/bin/bash /root/unifi-lets-encrypt-ssl-importer.sh -d $HOSTNAMEVAR
+/bin/bash /root/unifi-lets-encrypt-ssl-importer.sh -d ${DOMAINS[0]}
